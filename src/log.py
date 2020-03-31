9252b66578b07ee70f83cd09cfe2e1ebe8526955
Celian Raimbault <celian.raimbault@epita.fr>
2020-03-31 17:20:43 +0200

    + DQNAgent

--- src/log.py
+++ src/log.py
@@ -27,3 +27,31 @@ def as_yellow(s):
     '''
     return '\033[33m' + s + '\033[0m'
 
+
+class Logger:
+    '''
+        Used to display stats
+    '''
+    def __init__(self):
+        self.losses = []
+        self.rewards = []
+    
+    def show(self, epoch):
+        '''
+            Displays all informations gathered during training / testing
+        * Flushes all data
+        '''
+        avg_loss = sum(self.losses) / len(self.losses)
+        avg_reward = sum(self.rewards) / len(self.rewards)
+        min_reward = min(self.rewards)
+        max_reward = max(self.rewards)
+
+        self.losses.clear()
+        self.rewards.clear()
+
+        s = '| '
+        s += f'Epoch : {epoch:5d} | '
+        s += f'Average loss : {avg_loss:<7.4f} | '
+        s += f'Average reward : {avg_reward:<4.2f} | '
+        s += f'Min reward : {min_reward:<4.2f} | '
+        s += f'Max reward : {max_reward:<4.2f} | '
